7fb872aed8165ebb6a9f58261056446a4ae1cfbb
pinheiro767 <claudiapinheiro767@gmail.com>
2026-07-06 22:22:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,50 +4,34 @@ const ARQUIVOS_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-
-  "./assets/img/capa1.png",
-  "./assets/img/capa2.png",
-  "./assets/img/capa3.png",
-  "./assets/img/capa4.png",
-  "./assets/img/capa5.png",
-  "./assets/img/capa6.png",
-  "./assets/img/capa7.png",
-  "./assets/img/capa8.png",
-  "./assets/img/capa9.png",
-  "./assets/img/capa10.png",
-
+  "./assets/img/fundo-neuro.png",
+  "./assets/img/robo-neuro.png",
+  "./assets/img/capa-neuro.png",
+  "./assets/img/memoria-neuro.png",
+  "./assets/img/sudoku-neuro.png",
+  "./assets/img/cruzadas-neuro.png",
+  "./assets/img/quiz-neuro.png",
+  "./assets/img/cartas-neuro.png",
+  "./assets/img/trofeu-neuro.png",
   "./assets/audio/clique.mp3",
   "./assets/audio/sucesso.mp3",
   "./assets/audio/erro.mp3",
   "./assets/audio/vitoria.mp3",
   "./assets/audio/ambiente.mp3",
-
   "./assets/icons/icon-192.png",
   "./assets/icons/icon-512.png"
 ];
 
 self.addEventListener("install", evento => {
-  evento.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ARQUIVOS_CACHE))
-  );
+  evento.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ARQUIVOS_CACHE)));
 });
 
 self.addEventListener("activate", evento => {
   evento.waitUntil(
-    caches.keys().then(chaves => {
-      return Promise.all(
-        chaves
-          .filter(chave => chave !== CACHE_NAME)
-          .map(chave => caches.delete(chave))
-      );
-    })
+    caches.keys().then(chaves => Promise.all(chaves.filter(chave => chave !== CACHE_NAME).map(chave => caches.delete(chave))))
   );
 });
 
 self.addEventListener("fetch", evento => {
-  evento.respondWith(
-    caches.match(evento.request).then(resposta => {
-      return resposta || fetch(evento.request);
-    })
-  );
+  evento.respondWith(caches.match(evento.request).then(resposta => resposta || fetch(evento.request)));
 });
